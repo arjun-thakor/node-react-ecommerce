409c51d27386631df8db3bfab923e4b23972ad02
Parth Chauhan <parthchauhan2702@gmail.com>
2021-07-06 19:23:49 -0300

    Installed React Blueprint js Framework

---
+++ frontend/src/components/Breadcrums/Bread.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+// import ReactDOM from "react-dom";
+function Breadcrumb() { 
+    return (
+        <h1> Hello, World! </h1>
+    )
+}
+// const root = document.getElementById('root');
+// ReactDOM.render(<h1>!</h1>, root)
+export default Breadcrumb;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Breadcrumbs from "../src/components/Breadcrums/Bread.js";
+import Test from "../src/components/test.js";
+
 
 ReactDOM.render(
 	<React.StrictMode>
+		<Breadcrumbs />
+		<Test />
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
