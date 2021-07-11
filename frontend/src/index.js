a3de3328c8a8ea0ae03ac36a5bf3fb00ffe58882
Parth Chauhan <parthchauhan2702@gmail.com>
2021-07-11 15:46:17 -0300

    Implementing CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import App from "./App";
 import Breadcrumbs from "../src/components/Breadcrums/Bread.js";
 import Test from "../src/components/test.js";
